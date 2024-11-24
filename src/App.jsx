8baa87c0d3c01f1e9a6f12223ab3f8a3e69374da
Siddhartha Konduru <kondurus@usf.edu>
2024-11-24 09:46:42 -0500

    Setup root component with global state and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Task 1
+// App.jsx (Root Component)
 
-function App() {
-  const [count, setCount] = useState(0)
+// Imports react with useState, Gallery file, and the App css
+import { useState } from 'react'
+import Gallery from './Gallery'; 
+import './App.css'; 
+
+// Uses global state variable set to initial value, sets up the app structure
+const App = () => {
+  const [globalState, setGlobalAppState] = useState("valuesfirst value");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app-container">
+      <header>
+        <h1>{globalState}</h1>
+      </header>
+
+      <Gallery />
+    </div>
+  );
+};
 
 export default App
